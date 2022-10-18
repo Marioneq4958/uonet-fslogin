e0d6e42a54ee635119ad797e7d4b12ed2ed0d179
Marioneq4958 <marioneq4958@gmail.com>
2022-10-18 18:08:05 +0200

    Fix project name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="uonet-fslogin",
+    name="uonet_fslogin",
     version="0.1.0",
     packages=find_packages(),
     author="Marioneq4958",
